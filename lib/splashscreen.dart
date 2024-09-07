d7ae222cd9561d813d596ee81b9829b3185c8b0f
nabnab89 <lee89jiyeon@gmail.com>
2024-09-07 16:14:29 +0700

    Membuat Laman Home

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -10,10 +10,23 @@ class Splashscreen extends StatefulWidget {
 }
 
 class _SplashscreenState extends State<Splashscreen> {
+  bool _isLogoVisible = false; // Kontrol visibilitas logo
+  bool _isBungaVisible = false; // Kontrol visibilitas bunga
+
   @override
   void initState() {
     super.initState();
     startTimer();
+    Future.delayed(Duration(seconds: 1), () {
+      setState(() {
+        _isLogoVisible = true; // Mulai animasi fade-in untuk logo setelah 1 detik
+      });
+    });
+    Future.delayed(Duration(seconds: 1), () {
+      setState(() {
+        _isBungaVisible = true; // Mulai animasi fade-in untuk bunga setelah 2 detik
+      });
+    });
   }
 
   void startTimer() {
@@ -28,27 +41,56 @@ class _SplashscreenState extends State<Splashscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: <Widget>[
-          // Lottie animation in the center
-          Center(
-            child: Lottie.network(
-              'https://lottie.host/1aebefef-847a-4427-a534-43d29fa5291b/KNzElXfTB3.json',
-              fit: BoxFit.cover,
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerRight, 
+            end: Alignment.centerLeft, 
+            colors: [
+              Color(0xFFFAFFFF), // Warna putih sangat terang
+              Color(0xFFCDDDEF), // Warna biru muda
+            ],
           ),
-          // Image in the top-right corner
-          Positioned(
-            top: 20, // Adjust as needed
-            right: 20, // Adjust as needed
-            child: Image.asset(
-              'assets/img/Logo_YAKIN.png',
-              width: 100, // Adjust as needed
-              height: 100, // Adjust as needed
+        ),
+        child: Stack(
+          children: <Widget>[
+            // Lottie animation in the center
+            Center(
+              child: Lottie.network(
+                'https://lottie.host/1aebefef-847a-4427-a534-43d29fa5291b/KNzElXfTB3.json',
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-        ],
+            // Image in the top-right corner with fade-in animation
+            Positioned(
+              top: 20,
+              right: 20,
+              child: AnimatedOpacity(
+                opacity: _isLogoVisible ? 1.0 : 0.0,
+                duration: Duration(seconds: 2), // Durasi animasi fade-in
+                child: Image.asset(
+                  'assets/img/Logo_YAKIN.png',
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+            ),
+            // Image in the bottom-left corner with fade-in animation
+            Positioned(
+              bottom: 1,
+              left: 1,
+              child: AnimatedOpacity(
+                opacity: _isBungaVisible ? 1.0 : 0.0,
+                duration: Duration(seconds: 1), // Durasi animasi fade-in
+                child: Image.asset(
+                  'assets/img/Bunga.png',
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -11,14 +11,125 @@ class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Welcome"),
+      body: Stack(
+        children: <Widget>[
+          // Background with gradient
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
+                colors: [
+                  Color(0xFFFAFFFF), // Warna putih sangat terang
+                  Color(0xFFCDDDEF), // Warna biru muda
+                ],
+              ),
+            ),
+          ),
+          // Positioned image at the top-right corner
+          Positioned(
+            top: 2,
+            right: 1,
+            child: Image.asset(
+              'assets/img/Buble.png',
+              width: 350,
+              height: 350,
+            ),
+          ),
+          // Positioned Row with image and text at the top-left corner
+          Positioned(
+            top: 10,
+            right: 3,
+            child: Row(
+              children: [
+                Text(
+                  'Home',
+                  style: TextStyle(
+                    fontSize: 30, // Ukuran font
+                    fontWeight: FontWeight.bold, // Tebal teks
+                    color: Color.fromRGBO(9, 30, 58, 1.0),
+                  ),
+                ),
+                SizedBox(width: 10), // Jarak antara teks dan gambar
+                Image.asset(
+                  'assets/img/Home_img.png',
+                  width: 200,
+                  height: 150,
+                ),
+              ],
+            ),
+          ),
+          // Positioned Card below the Buble image
+          Positioned(
+            top: 280, // Adjust the top position based on the image height
+            right: 10,
+            left: 10, // Adjust the right position
+            child: Card(
+              elevation: 5, // For shadow effect
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8), // Rounded corners
+              ),
+              child: Container(
+                width: 300, // Width of the card
+                height: 150, // Height of the card
+                padding: EdgeInsets.all(10), // Padding inside the card
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/img/Home_menu1.png',
+                      width: 95,
+                      height: 95,
+                    ),
+                    Text(
+                      'Baca Kitab',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(9, 30, 58, 1.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 455, // Adjust the top position based on the image height
+            right: 10,
+            left: 10, // Adjust the right position
+            child: Card(
+              elevation: 5, // For shadow effect
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8), // Rounded corners
+              ),
+              child: Container(
+                width: 300, // Width of the card
+                height: 150, // Height of the card
+                padding: EdgeInsets.all(10), // Padding inside the card
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/img/Home_menu2.png',
+                      width: 95,
+                      height: 95,
+                    ),
+                    Text(
+                      'Penjelasan Kitab',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(9, 30, 58, 1.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: content(),
     );
-  }
-
-  Widget content() {
-    return Container();
   }
 }
